5c95cf055374818316600ba226208e571ede7cf6
duonglong1911 <you@example.com>
2021-08-25 22:22:48 +0700

    fixbug UI

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -7,64 +7,64 @@ class Login extends Component {
         return (
             <div>
                 <div className="login">
-                    <div className="box-root flex-flex flex-direction--column" style={{"min-height":"100vh","-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}}>
+                    <div className="box-root flex-flex flex-direction--column" style={{"minHeight":"100vh","WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}}>
                         <div className="loginbackground box-background--white padding-top--64">
                         <div className="loginbackground-gridContainer">
-                            <div className="box-root flex-flex" style={{"grid-area":"top / start / 8 / end"}}>
-                            <div className="box-root" style={{"background-image":"linear-gradient(                    white 0%,                    rgb(247, 250, 252) 33%                  )","-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            <div className="box-root flex-flex" style={{"gridArea":"top / start / 8 / end"}}>
+                            <div className="box-root" style={{"backgroundImage":"linear-gradient(                    white 0%,                    rgb(247, 250, 252) 33%                  )","WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"4 / 2 / auto / 5"}}>
+                            <div className="box-root flex-flex" style={{"gridArea":"4 / 2 / auto / 5"}}>
                             <div className="
                                 box-root
                                 box-divider--light-all-2
                                 animationLeftRight
                                 tans3s
-                            " style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            " style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"6 / start / auto / 2"}}>
-                            <div className="box-root box-background--blue800" style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            <div className="box-root flex-flex" style={{"gridArea":"6 / start / auto / 2"}}>
+                            <div className="box-root box-background--blue800" style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"7 / start / auto / 4"}}>
-                            <div className="box-root box-background--blue animationLeftRight" style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            <div className="box-root flex-flex" style={{"gridArea":"7 / start / auto / 4"}}>
+                            <div className="box-root box-background--blue animationLeftRight" style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"8 / 4 / auto / 6"}}>
+                            <div className="box-root flex-flex" style={{"gridArea":"8 / 4 / auto / 6"}}>
                             <div className="
                                 box-root
                                 box-background--gray100
                                 animationLeftRight
                                 tans3s
-                            " style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            " style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"2 / 15 / auto / end"}}>
+                            <div className="box-root flex-flex" style={{"gridArea":"2 / 15 / auto / end"}}>
                             <div className="
                                 box-root
                                 box-background--cyan200
                                 animationRightLeft
                                 tans4s
-                            " style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            " style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"3 / 14 / auto / end"}}>
-                            <div className="box-root box-background--blue animationRightLeft" style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            <div className="box-root flex-flex" style={{"gridArea":"3 / 14 / auto / end"}}>
+                            <div className="box-root box-background--blue animationRightLeft" style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"4 / 17 / auto / 20"}}>
+                            <div className="box-root flex-flex" style={{"gridArea":"4 / 17 / auto / 20"}}>
                             <div className="
                                 box-root
                                 box-background--gray100
                                 animationRightLeft
                                 tans4s
-                            " style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            " style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
-                            <div className="box-root flex-flex" style={{"grid-area":"5 / 14 / auto / 17"}}>
+                            <div className="box-root flex-flex" style={{"gridArea":"5 / 14 / auto / 17"}}>
                             <div className="
                                 box-root
                                 box-divider--light-all-2
                                 animationRightLeft
                                 tans3s
-                            " style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1"}} />
+                            " style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1"}} />
                             </div>
                         </div>
                         </div>
-                        <div className="box-root padding-top--24 flex-flex flex-direction--column" style={{"-webkit-flex-grow":"1","-ms-flex-grow":"1","flex-grow":"1","z-index":"9"}}>
+                        <div className="box-root padding-top--24 flex-flex flex-direction--column" style={{"WebkitFlexGrow":"1","msFlexGrow":"1","flexGrow":"1","zIndex":"9"}}>
                         <div className="
                             box-root
                             padding-top--48
